0bcab2b4a0323b3184425dc9dd8cbc1b6997dbc8
mettoboshi <mettoboshi.1108@gmail.com>
2015-03-16 16:40:59 +0900

    initial commit

---
+++ app.py
@@ -0,0 +1,22 @@
+import json
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+@app.route('/test/')
+def hello_test():
+    data = '{"b": "test"}'
+
+    print(data)
+    print(json.dumps(data))
+    return 'Hello World!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    # app.run()
